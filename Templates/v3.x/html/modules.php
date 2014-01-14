b5dc98e9daf7002d5bcd2943c631055e04a4bab3
Julio Noda <julio.noda@gmail.com>
2014-01-14 16:41:58 +0100

    Changed names and descriptions for template items

--- Templates/v3.x/html/modules.php
+++ Templates/v3.x/html/modules.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  Templates.protostar
+ * @package     151st.Site
+ * @subpackage  Templates.151st
  *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2014 151st Freedom Fighters. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
